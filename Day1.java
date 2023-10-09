ac9c63be13282148a7e2209c69d0b07815c1cc80
Miftah Faridl. S <115477714+Miffaridell@users.noreply.github.com>
2023-10-09 21:53:36 +0700

    Add files via upload

---
+++ Day1.java
@@ -0,0 +1,17 @@
+package day1;
+
+public class Day1 {
+
+    public static void main(String[] args) {
+        String nama = "MIFTAH FARIDL.S";
+        int umur = 19;
+        String alamat = "Galung";
+        String status = "HTS";
+
+        System.out.println("nama :" + nama);
+        System.out.println("umur :" + umur);
+        System.out.println("alamat :" + alamat);
+        System.out.println("status :" + status);
+    }
+
+}

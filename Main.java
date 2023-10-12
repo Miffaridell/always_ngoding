ecc47a6ac9618d89ff007fecf59912277661608d
Miftah Faridl. S <115477714+Miffaridell@users.noreply.github.com>
2023-10-12 23:48:43 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package day4;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int angka1 = 5;
+        int angka2 = 10;
+        int hasil = angka1 * angka2;
+        System.out.println("hasilnya :" + hasil);
+    }
+    
+}

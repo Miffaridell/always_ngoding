1e9f10022fb045251057fb34264fdc79028e80c6
Miftah Faridl. S <115477714+Miffaridell@users.noreply.github.com>
2023-10-14 02:53:06 +0800

    Add files via upload

---
+++ day5.java
@@ -0,0 +1,15 @@
+package day5;
+
+public class day5 {
+    
+    public static void main(String[] args) {
+        //luas segitiga//
+        int alas = 50;
+        int tinggi = 4;
+        int hasil = (alas * tinggi *1/2);
+        
+        
+        System.out.println("hasilnya :" + hasil);
+    }
+    
+}

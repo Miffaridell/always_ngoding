93c9ef63470842ad4bc4e5d019867e46cf8ec5df
Miftah Faridl. S <115477714+Miffaridell@users.noreply.github.com>
2023-10-15 21:20:01 +0800

    Add files via upload

---
+++ day6.java
@@ -0,0 +1,13 @@
+package day6;
+
+public class day6 {
+    
+    public static void main(String[] args) {
+         //menghitung jarak kota A ke b//
+        int kotaA = 50;
+        int kotaB = 80;
+        int hasil = kotaB - kotaA;
+        System.out.println("hasilnya :" +hasil)
+    }
+    
+}

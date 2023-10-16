56ddb971291cef8970ebe3f4060f0700f02e1d31
Miftah Faridl. S <115477714+Miffaridell@users.noreply.github.com>
2023-10-16 23:04:27 +0800

    Add files via upload

---
+++ day8.java
@@ -0,0 +1,9 @@
+
+public class day8 {
+    public static void main(String[] args) {
+        for(int i=0; i <= 5; i++){
+            System.out.println("MIFTAH FARIDL.S");
+        }
+    }
+}
+        

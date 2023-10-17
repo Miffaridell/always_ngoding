424355d7e81413fdd33f69806cdc19faea6f0aa8
Miftah Faridl. S <115477714+Miffaridell@users.noreply.github.com>
2023-10-17 23:51:45 +0800

    Add files via upload

---
+++ Day9.java
@@ -0,0 +1,14 @@
+package day9;
+
+public class Day9 {
+
+    public static void main(String[] args) {
+        int usia = 19;
+        if (usia >= 19) {
+            System.out.println("anda sudah dewasa");
+        } else {
+            System.out.println("anda belum dewasa");
+        }
+
+    }
+}

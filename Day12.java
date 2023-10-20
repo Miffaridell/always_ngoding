f2bcf4624266adc7a18b7b7d99f50166c482992c
Miftah Faridl. S <115477714+Miffaridell@users.noreply.github.com>
2023-10-20 23:01:35 +0700

    Add files via upload

---
+++ Day12.java
@@ -0,0 +1,18 @@
+
+package day12;
+
+import java.util.ArrayList;
+
+public class Day12 {
+
+    public static void main(String[] args) {
+     ArrayList <String> random = new ArrayList();
+        random.add("immiftah");
+        random.add("faridelll");
+        random.add("140604");
+        random.add("170");
+        random.add("78");
+        System.out.println(random);    
+    }
+    
+}

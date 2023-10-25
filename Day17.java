96402a6fd5749da97d54ab4cc279595ecff655d4
Miftah Faridl. S <115477714+Miffaridell@users.noreply.github.com>
2023-10-25 21:33:10 +0700

    Add files via upload

---
+++ Day17.java
@@ -0,0 +1,32 @@
+package day17;
+
+import java.util.Scanner;
+
+public class Day17 {
+
+    public static void main(String[] args) {
+        //int hari = 1;
+        //String stringhari;
+        //switch (hari) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan nama hari:");
+        String stringhari = scanner.nextLine();
+
+        switch (stringhari) {
+            case "senin":
+            case "selasa":
+            case "rabu":
+            case "kamis":
+            case "jumat":
+                System.out.println("normal day");
+                break;
+            case "sabtu":
+            case "minggu":
+                System.out.println("weekend");
+                break;
+            default:
+                System.out.println("invalid day");
+                break;
+        }
+    }
+}

bf7542bec8980242471eb81370c4da5a0b52442d
Miftah Faridl. S <115477714+Miffaridell@users.noreply.github.com>
2023-10-31 22:38:33 +0800

    Add files via upload

---
+++ Day23.java
@@ -0,0 +1,14 @@
+package day23;
+
+public class Day23 {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        int i = 0;
+        do {
+            System.out.println("numbers :" + numbers[i]);
+            i++;
+        } while (i < numbers.length);
+    }
+
+}

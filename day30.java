32b9fb84dfc0dd1bbfdbaa3a1e53d01fda0dddd4
Miftah Faridl. S <115477714+Miffaridell@users.noreply.github.com>
2023-11-07 08:04:34 -0800

    Add files via upload

---
+++ day30.java
@@ -0,0 +1,11 @@
+package day30;
+
+public class day30 {
+     public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        
+        for (int nomor : numbers) {
+            System.out.println(nomor);
+        }
+    }
+}

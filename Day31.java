3d4d22949acf4a9a29b5cc5d1d25af468175d613
Miftah Faridl. S <115477714+Miffaridell@users.noreply.github.com>
2023-11-09 10:01:12 -0800

    Add files via upload

---
+++ Day31.java
@@ -0,0 +1,14 @@
+package day31;
+
+public class Day31 {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        for (int nomor : numbers) {
+            int nilai = nomor * nomor;
+            System.out.println("nilai dari" + nomor + ":" + nilai);
+        }
+    }
+}
